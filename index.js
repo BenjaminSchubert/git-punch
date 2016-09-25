6c4a122d4db62fb6143a71305a44d6e7e5d0a9cd
Benjamin Schubert <ben.c.schubert@gmail.com>
2016-09-25 22:10:44 +0200

    updated link

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 
 
+var ghPages = "https://flagoul.github.io/HEIG_TWEB_2016_lab01/";
+var assets = ghPages + "/assets";
+
+
 app.set('port', (process.env.PORT || 5000));
 
 
@@ -9,7 +13,7 @@ app.set('views', __dirname + '/templates');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.render('index', { assets: "https://flagoul.github.io/HEIG_TWEB_2016_lab01/assets"});
+    response.render('index', { assets: assets, ghPages: ghPages});
 });
 
 app.listen(app.get('port'), function() {
