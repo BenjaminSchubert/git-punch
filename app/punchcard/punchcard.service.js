d53fdca0e0bdb19ed251adcf30a5b857935b8780
Benjamin Schubert <ben.c.schubert@gmail.com>
2016-10-17 17:32:59 +0200

    push

--- app/punchcard/punchcard.service.js
+++ app/punchcard/punchcard.service.js
@@ -13,21 +13,10 @@ app.factory('gstats.punchcard', function($http) {
 
         get commits() {
             return this.projects.then(function(result) {
-                count = 0;
                 return result.map(function(res) {
-                    var promise = new Promise(function(resolve) {
-                        setTimeout(function() {
-                            resolve(function() {
-                                return $http.get("api/punchcard/commits/" + res).then(function (data) {
-                                    return data.data;
-                                })
-                            });
-                        });
-                    });
-                    count += 250;
-
-                    return promise;
-
+                    return $http.get("api/punchcard/commits/" + res).then(function(data) {
+                        return data.data;
+                    })
                 });
             })
         }
